5ef1a316f77f3d4c8b5ed7e536900951da2cad6a
SarthakGangwar17 <sarthakgangwar1700@gmail.com>
2024-10-09 13:22:46 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+hypenedot='-e .'
+def getrequirement(file_path:str)->List[str]:
+    
+    
+    requirement=[]
+
+    with open(file_path) as file:
+        
+        requirement=file.readlines()
+        requirement=[r.replace('\n',"") for r in requirement]
+        if hypenedot in requirement:
+            requirement.remove(hypenedot)
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Sarthak Patel',
+    author_email='sarthakgangwar1700@gmail.com',
+    packages=find_packages(),
+    install_requires=getrequirement('requirement.txt')
+    
+
+
+
+)
