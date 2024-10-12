d2b1bb99b5af441eb354508bdad3711c36e15956
SarthakGangwar17 <sarthakgangwar1700@gmail.com>
2024-10-12 23:30:39 +0530

    Added

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -11,8 +11,8 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path=os.path.join("artifacts","model.pkl")
-            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            model_path=('artifacts\model.pkl')
+            preprocessor_path=('artifacts\preprocessor.pkl')
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
             data_scaled=preprocessor.transform(features)
@@ -27,7 +27,7 @@ class PredictPipeline:
 class CustomData:
     def __init__(  self,
         gender: str,
-        ethnicity: str,
+        race_ethnicity: str,
         parental_level_of_education,
         lunch: str,
         test_preparation_course: str,
@@ -36,7 +36,7 @@ class CustomData:
 
         self.gender = gender
 
-        self.ethnicity = ethnicity
+        self.race_ethnicity = race_ethnicity
 
         self.parental_level_of_education = parental_level_of_education
 
@@ -52,7 +52,7 @@ class CustomData:
         try:
             custom_data_input_dict = {
                 "gender": [self.gender],
-                "ethnicity": [self.ethnicity],
+                "race_ethnicity": [self.race_ethnicity],
                 "parental_level_of_education": [self.parental_level_of_education],
                 "lunch": [self.lunch],
                 "test_preparation_course": [self.test_preparation_course],
